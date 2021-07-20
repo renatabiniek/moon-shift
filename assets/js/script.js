dea3ecc5bf1fe9e47b0b17f406fb4185f2e2c90e
renatabiniek <renata.biniek@gmail.com>
2021-07-20 23:37:13 +0000

    Add js functions to open and close instructions modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,19 @@
-console.log("Hello Moon!")
+//functions for modals
+
+//when user clicks on "?" button, open the modal with instructions
+let modal = document.getElementById("instructions-modal");
+document.getElementById("open-btn").addEventListener("click", function() {
+modal.style.display = "block";
+})
+
+//when user clicks on "x" button, close the modal
+document.getElementsByClassName("close-modal")[0].addEventListener("click", function() {
+    modal.style.display = "none";
+    })
+
+//when user clicks anywhere else on the window, close the modal
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none"; 
+    }
+}
